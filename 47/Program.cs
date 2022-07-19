8563219c7760c801b118c625390ae7922a99b475
Nataly <17pn@mail.ru>
2022-07-19 17:10:01 +0300

    омашнее задание 7

---
+++ 47/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double array[,] = GetArray(int m, int n, 0, 10);
