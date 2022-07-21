e25eb792575f5b8fc4b51e554539695f845d10f1
Nataly <17pn@mail.ru>
2022-07-21 12:51:23 +0300

    домашнее задание 7

--- 47/Program.cs
+++ 47/Program.cs
@@ -5,9 +5,17 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9*/
 
-Console.WriteLine("Введите число строк: ");
+Console.WriteLine("Введите число строк и столбцов: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число столбцов: ");
 int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
 
-double array[,] = GetArray(int m, int n, 0, 10);
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++){
+        array[i, j] = rnd.NextDouble();
+        Console.Write(array[i, j]+ " ");
+    }
+    Console.WriteLine();
+}
