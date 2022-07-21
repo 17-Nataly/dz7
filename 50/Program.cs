4573ac9890584278ce63e21d0889840aff1f03cc
Nataly <17pn@mail.ru>
2022-07-21 13:10:58 +0300

    домашнее задание 7

--- 50/Program.cs
+++ 50/Program.cs
@@ -6,4 +6,29 @@
 8 4 2 4
 17 -> такого числа в массиве нет*/
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число строк и столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию элемента: ");
+int i0 = Convert.ToInt32(Console.ReadLine());
+int j0 = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++){
+        array[i, j] = rnd.Next(-10, 10);
+        
+        Console.Write(array[i, j]+ " ");      
+    }
+    Console.WriteLine();
+}
+
+if (i0 >= m || j0 >= n || i0 < 0 || j0 < 0) {
+    Console.WriteLine("Такого элемента нет");
+}
+else {
+    Console.WriteLine("Значение элемента {0}", array[i0,j0]);
+}
--- 52/Program.cs
+++ 52/Program.cs
@@ -5,4 +5,28 @@
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число строк и столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+        array[i, j] = rnd.Next(-10, 10);
+        
+        Console.Write(array[i, j]+ "  ");      
+    }
+    Console.WriteLine("");
+}
+
+for (int j = 0; j < n; j++) {
+    double sum = 0;
+    for (int i = 0; i < m; i++) {
+        sum += array[i, j];
+    }
+    Console.WriteLine("ср.арифметическое по столбцам {0}", sum / m);
+}
